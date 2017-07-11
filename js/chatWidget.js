d7d6588cb5f993810daf55a8dfac88aca0e55c96
Eric Griffis <eric.griffis@nyu.edu>
2017-07-11 12:02:53 -0400

    Remove unneccessary controller

--- js/chatWidget.js
+++ js/chatWidget.js
@@ -1,17 +1,11 @@
 import { viewName } from './viewName';
 
-// Add Clickable Logo
-class chatWidgetController {
-
-}
-
 export let chatWidgetConfig = {
   name: 'prmExploreMainAfter',
   config: {
     bindings: {
       parentCtrl: '<'
     },
-    controller: chatWidgetController,
     templateUrl: 'custom/' + viewName + '/html/chatWidget.html'
   }
 };
