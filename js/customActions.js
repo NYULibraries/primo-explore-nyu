128657a68e606d02ac4ee99c01cbec980e235fc7
Barnaby Alter <barnaby.alter@nyu.edu>
2017-08-21 15:43:10 -0400

    Updated icons for send/share options; [Finishes #150268089] [Finishes #150140735]

--- js/customActions.js
+++ js/customActions.js
@@ -6,7 +6,7 @@ export let customActionsConstant = {
       type: 'template',
       icon: {
           set: 'action',
-          name: 'ic_open_in_new_24px'
+          name: 'ic_description_24px'
       },
       action: "/primo_library/libweb/action/PushToAction.do?indx=1&doc={recordId}&recId={recordId}&docs={recordId}&pushToType=EndNote&fromEshelf=false"
     },
@@ -15,7 +15,7 @@ export let customActionsConstant = {
       type: 'template',
       icon: {
           set: 'action',
-          name: 'ic_open_in_new_24px'
+          name: 'ic_description_24px'
       },
       action: "/primo_library/libweb/action/PushToAction.do?indx=1&doc={recordId}&recId={recordId}&docs={recordId}&pushToType=RefWorks&fromEshelf=false"
     },
@@ -23,8 +23,8 @@ export let customActionsConstant = {
       name: "EasyBIB",
       type: 'template',
       icon: {
-          set: 'action',
-          name: 'ic_open_in_new_24px'
+          set: 'file',
+          name: 'ic_file_download_24px'
       },
       action: "/primo_library/libweb/action/PushToAction.do?indx=1&doc={recordId}&recId={recordId}&docs={recordId}&pushToType=EasyBIB&fromEshelf=false"
     },
@@ -32,8 +32,8 @@ export let customActionsConstant = {
       name: "RIS",
       type: "template",
       icon: {
-        set: "action",
-        name: "ic_visibility_24px"
+        set: "file",
+        name: "ic_file_download_24px"
       },
       action: "/primo_library/libweb/action/PushToAction.do?doc={recordId}&recId={recordId}&docs={recordId}&pushToType=RIS&fromEshelf=false"
     },
@@ -41,8 +41,8 @@ export let customActionsConstant = {
       name: "BibTeX",
       type: "template",
       icon: {
-        set: "content",
-        name: "ic_gesture_24px"
+        set: "file",
+        name: "ic_file_download_24px"
       },
       action: "/primo_library/libweb/action/PushToAction.do?doc={recordId}&recId={recordId}&docs={recordId}&pushToType=BibTeX&fromEshelf=false"
     }
