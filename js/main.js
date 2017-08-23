d8825303979a5ab440c8227971f041e02a293f25
Barnaby Alter <barnaby.alter@nyu.edu>
2017-08-23 17:12:03 -0400

    Hide default user options and add our own my account

--- js/main.js
+++ js/main.js
@@ -5,18 +5,33 @@ import { prmLogoAfterConfig } from './prmLogoAfter';
 import { prmExploreMainAfterConfig } from './prmExploreMainAfter';
 import { prmSearchResultAvailabilityLineAfterConfig } from './prmSearchResultAvailabilityLineAfter';
 import { prmNoSearchResultAfterConfig } from './prmNoSearchResultAfter';
-import { customActionsConstant } from './customActions'
+import { customActionsConstant } from './customActions';
 
-let app = angular.module('viewCustom', ['angularLoad', 'customActions']);
+angular
+  .module('customLibraryCardMenu', [])
+  .controller('customLibraryCardMenuController', ['$scope', '$window', ($scope, $window) => {
+    parentCtrl = () => {
+      return this.parentCtrl;
+    }
+    url = () => {
+      console.log('{{"urls.eshelf" | translate}}' + '/account');
+      let url = '{{"urls.eshelf" | translate}}' + '/account';
+      return url;
+    }
+  }])
+  .component('prmLibraryCardMenuAfter', {
+    bindings: {
+      parentCtrl: '<'
+    },
+    controller: 'customLibraryCardMenuController',
+    templateUrl: 'custom/' + viewName + '/html/library_card_menu.html'
+  });
+
+let app = angular.module('viewCustom', ['angularLoad', 'customActions', 'customLibraryCardMenu']);
 
 app.component(prmLogoAfterConfig.name, prmLogoAfterConfig.config)
    .component(prmExploreMainAfterConfig.name, prmExploreMainAfterConfig.config)
    .component(prmSearchResultAvailabilityLineAfterConfig.name, prmSearchResultAvailabilityLineAfterConfig.config)
    .component(prmNoSearchResultAfterConfig.name, prmNoSearchResultAfterConfig.config)
-  //  .controller('prmBriefResults', function ($scope, $controller) {
-  //   'use strict';
-  //   $controller('prmBriefResults', {$scope: $scope});
-  //   $scope.handleDetails=function(){}
-  // });
 
 app.constant(customActionsConstant.name, customActionsConstant.config)
