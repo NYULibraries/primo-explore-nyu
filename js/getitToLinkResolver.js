dc310465c8cef3cf9c83c9ab464628946b0d861e
Barnaby Alter <barnaby.alter@nyu.edu>
2017-09-05 15:55:57 -0400

    Moved link resolver functionality to npm package

---
+++ js/getitToLinkResolver.js
@@ -0,0 +1,15 @@
+export let getitToLinkResolverConfig = {
+  name: 'getitToLinkResolverConfig',
+  config: {
+    linkField: 'lln10',
+    linkText: 'Check Availability',
+    iconBefore: {
+      set: 'primo-ui',
+      icon: 'book-open'
+    },
+    iconAfter: {
+      set: 'primo-ui',
+      icon: 'open-in-new'
+    }
+  }
+};
--- js/main.js
+++ js/main.js
@@ -3,79 +3,14 @@ import 'primo-explore-custom-library-card-menu';
 import 'primo-explore-clickable-logo-to-any-link';
 import 'primo-explore-custom-no-search-results';
 import 'primo-explore-libraryh3lp-widget';
+import 'primo-explore-getit-to-link-resolver';
 
 import { viewName } from './viewName';
 import { customActionsConfig } from './customActions';
 import { customLibraryCardMenuItemsConfig } from './customLibraryCardMenu';
 import { clickableLogoLinkConfig } from './clickableLogoToAnyLink';
 import { libraryh3lpWidgetConfig } from './libraryh3lpWidget';
-
-angular
-  .module('getitToLinkResolver', [])
-  .factory('getitToLinkResolverService', ['getitToLinkResolverConfig', '$filter', function(getitToLinkResolverConfig, $filter) {
-    return {
-      translate: function(original) {
-        return original.replace(/\{(.+)\}/g, (match, p1) => $filter('translate')(p1));
-      },
-      config: {
-        linkField: getitToLinkResolverConfig.linkField,
-        linkText: getitToLinkResolverConfig.linkText,
-        iconBefore: getitToLinkResolverConfig.iconBefore,
-        iconAfter: getitToLinkResolverConfig.iconAfter,
-        showInBriefResults: getitToLinkResolverConfig.showInBriefResults
-      }
-    }
-  }])
-  .controller('getitToLinkResolverFullController', ['getitToLinkResolverService', '$scope', function(getitToLinkResolverService, $scope) {
-    this.$onInit = function() {
-      $scope.config = getitToLinkResolverService.config;
-    };
-    $scope.getitLink = () => {
-      try {
-        return this.parentCtrl.item.delivery.link.filter(link => link["displayLabel"] == getitToLinkResolverService.config.linkField)[0]["linkURL"];
-      } catch (e) {
-        return '';
-      }
-    };
-    $scope.translate = (original) => getitToLinkResolverService.translate(original);
-  }])
-  .controller('getitToLinkResolverBriefController', ['getitToLinkResolverService', '$scope', function(getitToLinkResolverService, $scope) {
-    this.$onInit = function() {
-      $scope.config = getitToLinkResolverService.config;
-    };
-    $scope.getitLink = () => {
-      try {
-        return this.parentCtrl.result.link[getitToLinkResolverService.config.linkField];
-      } catch (e) {
-        return '';
-      }
-    };
-    $scope.translate = (original) => getitToLinkResolverService.translate(original);
-  }])
-  .component('prmOpacAfter', {
-    bindings: {
-      parentCtrl: '<'
-    },
-    controller: 'getitToLinkResolverFullController',
-    template: '<a ng-href="{{ getitLink() }}" class="arrow-link check-avail-link check-avail-link-full" target="_blank">'+
-              '<prm-icon style="z-index:1" icon-type="svg" svg-icon-set="{{config.iconBefore.set}}" icon-definition="{{config.iconBefore.icon}}"></prm-icon>'+
-              ' {{ translate(config.linkText) }} '+
-              '<prm-icon style="z-index:1" icon-type="svg" svg-icon-set="{{config.iconAfter.set}}" icon-definition="{{config.iconAfter.icon}}"></prm-icon>'+
-              '<prm-icon link-arrow="" icon-type="svg" svg-icon-set="primo-ui" icon-definition="chevron-right"></prm-icon>'+
-              '</a> '
-  })
-  .component('prmSearchResultAvailabilityLineAfter', {
-    bindings: {
-      parentCtrl: '<'
-    },
-    controller: 'getitToLinkResolverBriefController',
-    template: '<a ng-if="config.showInBriefResults" ng-href="{{ getitLink() }}" class="arrow-link check-avail-link check-avail-link-brief" target="_blank">'+
-              '<prm-icon style="z-index:1" icon-type="svg" svg-icon-set="{{config.iconBefore.set}}" icon-definition="{{config.iconBefore.icon}}"></prm-icon>'+
-              ' {{ translate(config.linkText) }} '+
-              '<prm-icon style="z-index:1" icon-type="svg" svg-icon-set="{{config.iconAfter.set}}" icon-definition="{{config.iconAfter.icon}}"></prm-icon>'+
-              '<prm-icon link-arrow="" icon-type="svg" svg-icon-set="primo-ui" icon-definition="chevron-right"></prm-icon>'+
-              '</a> '
-  });
+import { getitToLinkResolverConfig } from './getitToLinkResolver'
 
 let app = angular.module('viewCustom', [
                                         'angularLoad',
@@ -92,19 +27,11 @@ app
   .constant(customLibraryCardMenuItemsConfig.name, customLibraryCardMenuItemsConfig.config)
   .constant(clickableLogoLinkConfig.name, clickableLogoLinkConfig.config)
   .constant(libraryh3lpWidgetConfig.name, libraryh3lpWidgetConfig.config)
+  .constant(getitToLinkResolverConfig.name, getitToLinkResolverConfig.config)
   .value('customNoSearchResultsTemplateUrl', 'custom/' + viewName + '/html/noSearchResults.html')
-
-
-app.constant('getitToLinkResolverConfig', {
-  linkField: 'lln10',
-  linkText: 'Check Availability',
-  iconBefore: {
-    set: 'primo-ui',
-    icon: 'book-open'
-  },
-  iconAfter: {
-    set: 'primo-ui',
-    icon: 'open-in-new'
-  },
-  showInBriefResults: false
-});
+  // .component('prmSearchResultAvailabilityLineAfter', {
+  //   template: '<getit-to-link-resolver-brief></getit-to-link-resolver-brief>'
+  // })
+  .component('prmOpacAfter', {
+    template: '<getit-to-link-resolver-full></getit-to-link-resolver-full>'
+  })
