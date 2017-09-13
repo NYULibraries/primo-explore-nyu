5456ddeac080ba08c926da902f38b957f429315e
Barnaby Alter <barnaby.alter@nyu.edu>
2017-09-13 13:13:14 -0400

    Abstract out nyuEshelf functionality into npm package

--- js/main.js
+++ js/main.js
@@ -4,14 +4,15 @@ import 'primo-explore-clickable-logo-to-any-link';
 import 'primo-explore-custom-no-search-results';
 import 'primo-explore-libraryh3lp-widget';
 import 'primo-explore-getit-to-link-resolver';
+import 'primo-explore-nyu-eshelf';
 
 import { viewName } from './viewName';
 import { customActionsConfig } from './customActions';
 import { customLibraryCardMenuItemsConfig } from './customLibraryCardMenu';
 import { clickableLogoLinkConfig } from './clickableLogoToAnyLink';
 import { libraryh3lpWidgetConfig } from './libraryh3lpWidget';
-import { getitToLinkResolverConfig } from './getitToLinkResolver'
-import { nyuEshelf } from './nyuEshelf'
+import { getitToLinkResolverConfig } from './getitToLinkResolver';
+import { nyuEshelfConfig } from './nyuEshelf';
 
 let app = angular.module('viewCustom', [
                                         'angularLoad',
@@ -30,12 +31,13 @@ app
   .constant(clickableLogoLinkConfig.name, clickableLogoLinkConfig.config)
   .constant(libraryh3lpWidgetConfig.name, libraryh3lpWidgetConfig.config)
   .constant(getitToLinkResolverConfig.name, getitToLinkResolverConfig.config)
+  .constant(nyuEshelfConfig.name, nyuEshelfConfig.config)
   .value('customNoSearchResultsTemplateUrl', 'custom/' + viewName + '/html/noSearchResults.html')
-  .component('prmSearchResultAvailabilityLineAfter', {
-    template: '<nyu-eshelf></nyu-eshelf>'
-  })
   .component('prmOpacAfter', {
     template: '<getit-to-link-resolver-full></getit-to-link-resolver-full>'
+  })
+  .component('prmSearchResultAvailabilityLineAfter', {
+    template: '<nyu-eshelf></nyu-eshelf>'
   })
   .component('prmSearchBookmarkFilterAfter', {
     template: '<nyu-eshelf-toolbar></nyu-eshelf-toolbar>'
