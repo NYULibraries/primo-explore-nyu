b6ca2a877b14a1aae7b6dc9f3bc6be30d6019875
Barnaby Alter <barnaby.alter@nyu.edu>
2017-10-02 16:42:00 -0400

    Remove customizations already in CENTRAL_PACKAGE

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 import 'primo-explore-custom-actions';
 import 'primo-explore-custom-library-card-menu';
 import 'primo-explore-clickable-logo-to-any-link';
-import 'primo-explore-custom-no-search-results';
 import 'primo-explore-libraryh3lp-widget';
 import 'primo-explore-getit-to-link-resolver';
 import 'primo-explore-nyu-eshelf';
@@ -15,10 +14,10 @@ import { getitToLinkResolverConfig } from './getitToLinkResolver';
 import { nyuEshelfConfig } from './nyuEshelf';
 
 let app = angular.module('viewCustom', [
+                                        'angularLoad',
                                         'customActions',
                                         'customLibraryCardMenu',
                                         'clickableLogoToAnyLink',
-                                        'customNoSearchResults',
                                         'libraryh3lpWidget',
                                         'getitToLinkResolver',
                                         'nyuEshelf'
@@ -31,7 +30,6 @@ app
   .constant(libraryh3lpWidgetConfig.name, libraryh3lpWidgetConfig.config)
   .constant(getitToLinkResolverConfig.name, getitToLinkResolverConfig.config)
   .constant(nyuEshelfConfig.name, nyuEshelfConfig.config)
-  .value('customNoSearchResultsTemplateUrl', 'custom/' + viewName + '/html/noSearchResults.html')
   .component('prmFullViewServiceContainerAfter', {
     template: '<getit-to-link-resolver-full></getit-to-link-resolver-full>'
   })
