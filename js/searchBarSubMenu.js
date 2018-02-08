0f1d99ce06a16e16b6ec157ff0e6fbbb16ad2a94
CircleCI-NYULib <barnaby.alter@nyu.edu>
2018-02-08 14:03:34 -0500

    Clean up markup and allow hide-xs to be conditional

--- js/searchBarSubMenu.js
+++ js/searchBarSubMenu.js
@@ -8,7 +8,8 @@ export let searchBarSubMenuItemsConfig = {
       icon: {
         set: 'navigation',
         icon: 'ic_arrow_back_24px'
-      }
+      },
+      show_xs: true
     },
     {
       name: "Provide Feedback",
@@ -48,17 +49,15 @@ angular
   }])
   .component('searchBarSubMenu', {
     controller: 'searchBarSubMenuController',
-    // template: `<div class="layout-align-end-center layout-row flex-noshrink search-bar-sub-menu">
-    //             <ul>
-    //               <li ng-repeat="item in items">
-    //               <button aria-label="{{ translate(item.description) }}" ng-click="goToUrl(translate(item.action))" class="button-with-icon zero-margin md-button md-small {{item.cssClasses}}" type="button">
-    //                 <md-tooltip md-direction="bottom" md-delay="500">{{ translate(item.description) }}</md-tooltip><prm-icon style="z-index:1" icon-type="svg" svg-icon-set="{{item.icon.set}}" icon-definition="{{item.icon.icon}}"></prm-icon>
-    //                 <span class="custom_search_bookmark_filter_item hide-xs">{{ translate(item.name) }}</span>
-    //               </button>
-    //               </li>
-    //             </ul>
-    //           </div>`
-    template: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique, ante a facilisis dapibus, est ex sodales enim, ut dignissim lacus enim in arcu. Aenean sed molestie lorem. Donec tempus convallis porttitor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec enim metus, pretium eu bibendum quis, bibendum id nibh. In hac habitasse platea dictumst. Aenean et dolor metus. Nulla pulvinar lorem ut quam blandit posuere. Ut arcu enim, blandit ac odio eget, tincidunt mattis dui. Aenean a rutrum felis, sit amet pretium justo. Nullam ut diam id mauris lacinia cursus. `
-
+    template: `<div class="layout-align-end-center layout-row flex search-bar-sub-menu">
+                <ul>
+                  <li ng-repeat="item in items">
+                  <button aria-label="{{ translate(item.description) }}" ng-click="goToUrl(translate(item.action))" class="button-with-icon zero-margin md-button md-small {{item.cssClasses}}" type="button">
+                    <md-tooltip md-direction="bottom" md-delay="500">{{ translate(item.description) }}</md-tooltip><prm-icon style="z-index:1" icon-type="svg" svg-icon-set="{{item.icon.set}}" icon-definition="{{item.icon.icon}}"></prm-icon>
+                    <span class="search-bar-sub-menu-item" ng-class="(item.show_xs) ? '' : 'hide-xs'">{{ translate(item.name) }}</span>
+                  </button>
+                  </li>
+                </ul>
+              </div>`
 
   });
