1918d68e4c498ea2c521e081aecbe16c3e044af4
barnabyalter <barnaby.alter@nyu.edu>
2018-03-09 11:59:00 -0500

    Have to hardcode viewName into customActions

--- js/customActions.js
+++ js/customActions.js
@@ -1,4 +1,3 @@
-import { viewName } from './viewName';
 export let customActionsConfig = {
   name: 'customActions',
   template: `
@@ -13,24 +12,24 @@ export let customActionsConfig = {
                 index=4
                 icon="ic_description_24px"
                 icon-set="action"
-                link="http://nyu-pushtocite.herokuapp.com/?calling_system=primo&institution={viewName}&local_id={pnx.search.recordid[0]}&cite_to=endnote" />
+                link="http://nyu-pushtocite.herokuapp.com/?calling_system=primo&institution=NYU&local_id={pnx.search.recordid[0]}&cite_to=endnote" />
             <custom-action name="export_refworks"
                   label="RefWorks"
                   index=5
                   icon="ic_description_24px"
                   icon-set="action"
-                  link="http://nyu-pushtocite.herokuapp.com/?calling_system=primo&institution={viewName}&local_id={pnx.search.recordid[0]}&cite_to=refworks" />
+                  link="http://nyu-pushtocite.herokuapp.com/?calling_system=primo&institution=NYU&local_id={pnx.search.recordid[0]}&cite_to=refworks" />
             <custom-action name="download_ris"
                   label="RIS"
                   index=6
                   icon="ic_file_download_24px"
                   icon-set="file"
-                  link="http://nyu-pushtocite.herokuapp.com/?calling_system=primo&institution={viewName}&local_id={pnx.search.recordid[0]}&cite_to=ris" />
+                  link="http://nyu-pushtocite.herokuapp.com/?calling_system=primo&institution=NYU&local_id={pnx.search.recordid[0]}&cite_to=ris" />
             <custom-action name="download_bibtex"
                   label="BibTeX"
                   index=7
                   icon="ic_file_download_24px"
                   icon-set="file"
-                  link="http://nyu-pushtocite.herokuapp.com/?calling_system=primo&institution={viewName}&local_id={pnx.search.recordid[0]}&cite_to=bibtex" />
+                  link="http://nyu-pushtocite.herokuapp.com/?calling_system=primo&institution=NYU&local_id={pnx.search.recordid[0]}&cite_to=bibtex" />
               `
 };
