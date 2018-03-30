88effa705864dee2b351f4d96c93d810a0fbd6c6
Elliott Grieco <elliott.grieco@gmail.com>
2018-03-30 10:40:20 -0400

    Adds basic analytics functionality

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ import 'primo-explore-libraryh3lp-widget';
 import 'primo-explore-getit-to-link-resolver';
 import 'primo-explore-nyu-eshelf';
 import 'primo-explore-search-bar-sub-menu';
+import 'angulartics';
+import 'angulartics-google-tag-manager';
 
 import { viewName } from './viewName';
 import { customActionsConfig } from './customActions';
@@ -23,7 +25,9 @@ let app = angular.module('viewCustom', [
                                         'libraryh3lpWidget',
                                         'getitToLinkResolver',
                                         'nyuEshelf',
-                                        'searchBarSubMenu'
+                                        'searchBarSubMenu',
+                                        'angulartics',
+                                        'angulartics.google.tagmanager'
                                       ]);
 
 app
@@ -48,3 +52,35 @@ app
   .component('prmSearchBarAfter', {
     template: '<search-bar-sub-menu></search-bar-sub-menu>'
   })
+
+
+
+// Google analytics
+const s1 = document.createElement('script');
+s1.src = "https://www.googletagmanager.com/gtag/js?id=UA-55461631-20";
+document.head.appendChild(s1);
+
+const s2 = document.createElement('script');
+s2.type = 'text/javascript';
+const code = `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'UA-55461631-20');`;
+
+try {
+  s2.appendChild(document.createTextNode(code));
+  document.head.appendChild(s2);
+} catch (e) {
+  s2.text = code;
+  document.body.appendChild(s2);
+}
+
+// <!-- Global site tag (gtag.js) - Google Analytics -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=UA-55461631-20"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+//
+//   gtag('config', 'UA-55461631-20');
+// </script>
