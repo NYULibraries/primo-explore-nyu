c63b5bcb05cbee6881a8f6ca6fb7970eaafb24de
BAlter <barnaby.alter@nyu.edu>
2018-06-13 16:15:23 -0400

    Remove back to Bobcat classic and beta references

--- js/searchBarSubMenu.js
+++ js/searchBarSubMenu.js
@@ -2,16 +2,6 @@ export let searchBarSubMenuItemsConfig = {
   name: 'searchBarSubMenuItems',
   config: [
     {
-      name: "Back to BobCat Classic",
-      description: "Back to BobCat Classic",
-      action: "http://bobcat.library.nyu.edu/primo_library/libweb/action/search.do?vid=" + "NYU-BKUP",
-      icon: {
-        set: 'navigation',
-        icon: 'ic_arrow_back_24px'
-      },
-      show_xs: true
-    },
-    {
       name: "Provide Feedback",
       description: "Provide Feedback",
       action: "https://nyu.qualtrics.com/jfe/form/SV_blQ3OFOew9vl6Pb?Source=NYU",
