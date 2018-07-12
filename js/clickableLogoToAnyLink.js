398baf443b9d3df2dfcf2debdf557fb8aa76ed93
Elliott Grieco <elliott.grieco@nyu.edu>
2018-07-12 13:50:44 -0400

    Upgrade logo link

--- js/clickableLogoToAnyLink.js
+++ js/clickableLogoToAnyLink.js
@@ -2,6 +2,7 @@ export let clickableLogoLinkConfig = {
   name: 'clickableLogoLinkConfig',
   config: {
     url: '{urls.library}',
-    altText: '{nui.header.logoAlt}'
+    altText: '{nui.header.logoAlt}',
+    iconLink:'https://library.nyu.edu/assets/images/nyulibraries-logo.svg'
   }
 };
