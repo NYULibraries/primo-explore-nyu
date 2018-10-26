69ea9beb39e7e0ba69cad6fb92cc6ddb9ec1d5b4
BAlter <barnaby.alter@nyu.edu>
2018-10-26 16:28:19 -0400

    Point custom actions to new cite.library url

--- js/customActions.js
+++ js/customActions.js
@@ -12,24 +12,24 @@ export let customActionsConfig = {
                 index=4
                 icon="ic_description_24px"
                 icon-set="action"
-                link="http://nyu-pushtocite.herokuapp.com/?calling_system=primo&institution=NYU&local_id={pnx.search.recordid[0]}&cite_to=endnote" />
+                link="https://cite.library.nyu.edu/{pnx.search.recordid[0]}?calling_system=primo&institution=NYU&cite_to=endnote" />
             <custom-action name="export_refworks"
                   label="RefWorks"
                   index=5
                   icon="ic_description_24px"
                   icon-set="action"
-                  link="http://nyu-pushtocite.herokuapp.com/?calling_system=primo&institution=NYU&local_id={pnx.search.recordid[0]}&cite_to=refworks" />
+                  link="https://cite.library.nyu.edu/{pnx.search.recordid[0]}?calling_system=primo&institution=NYU&cite_to=refworks" />
             <custom-action name="download_ris"
                   label="RIS"
                   index=6
                   icon="ic_file_download_24px"
                   icon-set="file"
-                  link="http://nyu-pushtocite.herokuapp.com/?calling_system=primo&institution=NYU&local_id={pnx.search.recordid[0]}&cite_to=ris" />
+                  link="https://cite.library.nyu.edu/{pnx.search.recordid[0]}?calling_system=primo&institution=NYU&cite_to=ris" />
             <custom-action name="download_bibtex"
                   label="BibTeX"
                   index=7
                   icon="ic_file_download_24px"
                   icon-set="file"
-                  link="http://nyu-pushtocite.herokuapp.com/?calling_system=primo&institution=NYU&local_id={pnx.search.recordid[0]}&cite_to=bibtex" />
+                  link="https://cite.library.nyu.edu/{pnx.search.recordid[0]}?calling_system=primo&institution=NYU&cite_to=bibtex" />
               `
 };
