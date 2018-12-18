8dd677c712d83ba52462bbc8a2550fbc957c2a18
Elliott Grieco <elliott.grieco@nyu.edu>
2018-12-18 12:14:28 -0500

    Fixes reveal/hide functionality to match spec

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@ app
   .component('prmSearchBarAfter', {
     template: '<search-bar-sub-menu></search-bar-sub-menu>'
   })
-  .constant('requestUnavailableConfig', {
+  .constant('customRequests', {
     ezBorrowUrl: 'http://login.library.nyu.edu/ezborrow/nyu',
     illiadUrl: 'http://ill.library.nyu.edu',
     authorizedStatuses: {
@@ -59,33 +59,31 @@ app
     bindings: {
       parentCtrl: '<',
     },
-    require: {
-      primoExploreCtrl: '^primoExplore'
-    },
     template: `
-      <div class="md-list-item-text">
-        <span ng-if="$ctrl.unavailable && !$ctrl.loggedIn && !$ctrl.user && !$ctrl.userFailure">Retrieving request options...</span>
-        <span ng-if="$ctrl.unavailable && $ctrl.userFailure">Unable to retrieve request options</span>
-        <a ng-if="$ctrl.unavailable && $ctrl.ezBorrowEligible" href="{{ $ctrl.ezBorrowLink }}" target="_blank">Request E-ZBorrow </a>
-        <a ng-if="$ctrl.unavailable && !$ctrl.ezBorrowEligible && $ctrl.illEligible" href="{{ $ctrl.illLink }}" target="_blank">Request ILL</a>
+      <div class="md-list-item-text" ng-if="$ctrl.allUnavailable">
+        <span ng-if="$ctrl.showLoadingText">Retrieving request options...</span>
+        <span ng-if="$ctrl.showFailureText">Unable to retrieve request options</span>
+        <a ng-if="$ctrl.showEZBorrowLink" href="{{ $ctrl.ezBorrowLink }}" target="_blank">Request E-ZBorrow </a>
+        <a ng-if="$ctrl.showIllLink" href="{{ $ctrl.illLink }}" target="_blank">Request ILL</a>
         <a ng-if="!$ctrl.loggedIn" ng-click="$ctrl.handleLogin($event)">Login to see request options</a>
       </div>
     `
-  })
-  .component('prmLocationItemsAfter', {
-    controller: checkUnavailabilityController,
-    bindings: {
-      parentCtrl: '<',
-    }
   })
   // Injects prmAuthentication's handleLogin as a global service
   .service('customLoginService', ['$window', '$http', function($window, $http) {
     const svc = this;
     svc.store = {
       user: undefined,
+      login: undefined,
+      logout: undefined,
     }
 
+
     svc.fetchPDSUser = (store) => {
+      store.user = { id: '123456', 'bor-status': '55'};
+      const later = (delay, value) => new Promise((resolve) => setTimeout(resolve, delay, value));
+      return later(1000, store.user);
+      // return Promise.reject('something went wrong');
       // source: https://gist.github.com/rendro/525bbbf85e84fa9042c2
       const cookies = $window.document.cookie
         .split(';')
@@ -99,33 +97,37 @@ app
           }
         }, {});
 
-      cookies.PDS_HANDLE = '13122018162114816496745208944470';
-      return $http.get(`https://pdsdev.library.nyu.edu/pds?func=get-attribute&attribute=bor_info&pds_handle=${cookies.PDS_HANDLE}`)
+      return $http.get(`https://pdsdev.library.nyu.edu/pds?func=get-attribute&attribute=bor_info&pds_handle=${cookies.PDS_HANDLE}`, {
+        timeout: 6000,
+      })
         .then(response => {
           const xml = response.data;
           const getXMLProp = prop => (new $window.DOMParser).parseFromString(xml, 'text/xml').querySelector(prop).textContent
-          return ['id', 'bor-status'].reduce((res, prop) => Object.assign(res, {
+          const user = ['id', 'bor-status'].reduce((res, prop) => Object.assign(res, {
             [prop]: getXMLProp(prop)
           }), {});
+
+          store.user = user;
+          return user;
         })
     }
 
     return {
-      login: () => {},
-      logout: () => {},
+      setLogin: fxn => { svc.store.login = fxn; return fxn; },
+      setLogout: fxn => { svc.store.logout = fxn; return fxn; },
+
+      // Written as a function to encapsulate changable login/logout functions
+      login: () => svc.store.login(),
+      logout: () => svc.store.logout(),
       fetchPDSUser: () => svc.store.user ? Promise.resolve(svc.store.user) : svc.fetchPDSUser(svc.store),
     };
   }])
-  .service('checkUnavailableService', function() {
+  .service('availabilityService', function() {
     const svc = this;
-    svc.store = {
-      status: undefined,
-    };
-
-    svc.isUnavailable = items => items.every(item => {
-      const hasPattern = (patterns, target) => patterns.some(str => target.match(new RegExp(str)))
+    svc.hasPattern = (patterns, target) => patterns.some(str => target.match(new RegExp(str)));
+    svc.checkIsAvailable = item => {
       const [circulationStatus, ...otherStatusFields] = item.itemFields;
-      return hasPattern([
+      return !svc.hasPattern([
         /Requested/g,
         /^\d{2}\/\d{2}\/\d{2}/g, // starts with dd/dd/dd
         'Requested',
@@ -137,15 +139,15 @@ app
         'Request ILL',
         'On Order',
       ], circulationStatus);
-    });
+    };
+
+    svc.state = {};
+    svc.setState = newState => Object.assign(svc.state, newState);
 
     return {
-      checkUnavailable(items) {
-        if (items !== undefined) {
-          svc.status = svc.isUnavailable(items) ? 'unavailable' : 'available';
-        }
-        return svc.status === 'unavailable';
-      },
+      checkIsAvailable: svc.checkIsAvailable,
+      getState: () => svc.state,
+      setState: svc.setState,
     }
   })
   .component('prmAuthenticationAfter', {
@@ -154,15 +156,6 @@ app
       parentCtrl: '<',
     }
   })
-
-authenticationController.$inject = ['customLoginService']
-function authenticationController(customLoginService) {
-  const ctrl = this;
-  ctrl.$onInit = function () {
-    customLoginService.login = ctrl.parentCtrl.handleLogin.bind(ctrl.parentCtrl);
-    customLoginService.logout = ctrl.parentCtrl.handleLogout.bind(ctrl.parentCtrl);
-  };
-}
 
 app.run(runBlock);
 
@@ -176,18 +169,17 @@ function runBlock(gaInjectionService, nyuEshelfService) {
   nyuEshelfService.initEshelf();
 }
 
-checkUnavailabilityController.$inject = ['requestUnavailableConfig', 'checkUnavailableService'];
-function checkUnavailabilityController(config, checkUnavailableService) {
+authenticationController.$inject = ['customLoginService']
+function authenticationController(customLoginService) {
   const ctrl = this;
-  const parentCtrl = ctrl.parentCtrl;
-
-  ctrl.$doCheck = function () {
-    checkUnavailableService.checkUnavailable(parentCtrl.currLoc ? parentCtrl.currLoc.items : []);
-  }
+  ctrl.$onInit = function () {
+    customLoginService.setLogin(ctrl.parentCtrl.handleLogin.bind(ctrl.parentCtrl));
+    customLoginService.setLogout(ctrl.parentCtrl.handleLogout.bind(ctrl.parentCtrl));
+  };
 }
 
-requestUnavailableController.$inject = ['requestUnavailableConfig', 'customLoginService', 'checkUnavailableService', '$window']
-function requestUnavailableController(config, customLoginService, checkUnavailableService, $window) {
+requestUnavailableController.$inject = ['customRequests', 'customLoginService', 'availabilityService', '$window', '$element']
+function requestUnavailableController(config, customLoginService, availabilityService, $window, $element) {
   const ctrl = this;
   const parentCtrl = ctrl.parentCtrl;
 
@@ -199,39 +191,50 @@ function requestUnavailableController(config, customLoginService, checkUnavailab
 
   ctrl.illLinkGenerator = (base, openurl) => `${base}/illiad/illiad.dll/OpenURL?${$window.encodeURIComponent(openurl)}`
 
-  ctrl.revealDefaultRequestOptions = () => {
-    angular.element(document.querySelector('prm-location-items .md-list-item-text div:nth-child(3)')).css({
-      display: 'block'
-    });
+  ctrl.revealRequest = (idx) => $element.parent().parent().queryAll('.md-list-item-text')[idx].children().eq(2).css({ display: 'block' });
+
+  ctrl.showLinks = ({ user, currLocItems, pnx, allUnavailable }) => {
+    const isBook = ['BOOK', 'BOOKS'].some(type => pnx.addata.ristype.indexOf(type) > -1);
+    const borStatus = user && user['bor-status'];
+    const ezborrow = isBook && allUnavailable && config.authorizedStatuses.ezBorrow.indexOf(borStatus) > -1;
+    const ill = !ezborrow && allUnavailable && config.authorizedStatuses.ill.indexOf(borStatus) > -1;
+
+    return {
+      ezborrow,
+      ill
+    };
   }
 
-  ctrl.hideDefaultRequestOptions = () => {
-    angular.element(document.querySelector('prm-location-items .md-list-item-text div:nth-child(3)')).css({
-      display: 'none'
-    });
+  ctrl.handleLogin = function (event) {
+    customLoginService.login();
+    event.stopPropagation();
   }
 
-  ctrl.$onInit = function() {
+  ctrl.$onInit = () => {
     ctrl.title = parentCtrl.item.pnx.addata.btitle ? parentCtrl.item.pnx.addata.btitle[0] : '';
     ctrl.author = parentCtrl.item.pnx.addata.au ? parentCtrl.item.pnx.addata.au[0] : '';
     ctrl.ezBorrowLink = ctrl.EZBorrowLinkGenerator(config.ezBorrowUrl, ctrl.title, ctrl.author);
-    ctrl.handleLogin = function(event) {
-      customLoginService.login();
-      event.stopPropagation();
-    }
+
+    const availabilities = ctrl.parentCtrl.currLoc.items.map(availabilityService.checkIsAvailable);
+    availabilities.forEach((isAvailable, idx) => isAvailable ? ctrl.revealRequest(idx) : null);
+    ctrl.allUnavailable = availabilities.every(status => status === false);
 
     ctrl.loggedIn = !parentCtrl.userSessionManagerService.isGuest();
-    ctrl.unavailable = checkUnavailableService.checkUnavailable();
 
     if (ctrl.loggedIn) {
       customLoginService.fetchPDSUser()
         .then(user => {
           ctrl.user = user;
 
-          const isBook = ['BOOK', 'BOOKS'].some(type => parentCtrl.item.pnx.addata.ristype.indexOf(type) > -1);
-          const borStatus = ctrl.user && ctrl.user['bor-status'];
-          ctrl.illEligible = ctrl.unavailable && config.authorizedStatuses.ill.indexOf(borStatus) > -1;
-          ctrl.ezBorrowEligible = isBook && ctrl.unavailable && config.authorizedStatuses.ezBorrow.indexOf(borStatus) > -1;
+          const { ezborrow, ill } = ctrl.showLinks({
+            user: ctrl.user,
+            pnx: parentCtrl.item.pnx,
+            currLocItems: parentCtrl.currLoc.items,
+            allUnavailable: ctrl.allUnavailable,
+          });
+
+          ctrl.showEZBorrowLink = ezborrow;
+          ctrl.showIllLink = ill;
         },
         rejectedResponse => {
           console.error(rejectedResponse);
@@ -240,13 +243,8 @@ function requestUnavailableController(config, customLoginService, checkUnavailab
     }
   }
 
-  ctrl.$doCheck = function() {
-    ctrl.unavailable = checkUnavailableService.checkUnavailable();
-    console.log(ctrl.unavailable, 'unavailable?')
-    if (!ctrl.unavailable) {
-      ctrl.revealDefaultRequestOptions();
-    } else {
-      ctrl.hideDefaultRequestOptions();
-    }
-  }
+  ctrl.$doCheck = () => {
+    ctrl.showLoadingText = ctrl.loggedIn && !ctrl.user && !ctrl.userFailure;
+    ctrl.showFailureText = ctrl.userFailure;
+  };
 }
