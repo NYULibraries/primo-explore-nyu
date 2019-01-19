02f749957e3db92233dfabce66741d87b34ad0d3
Elliott Grieco <elliott.grieco@nyu.edu>
2019-01-18 23:22:00 -0500

    Updates config for new custom-requests

--- js/customRequestsConfig.js
+++ js/customRequestsConfig.js
@@ -1,7 +1,7 @@
 export default {
   name: 'primoExploreCustomRequestsConfig',
   config: {
-    buttonIds: ['login', 'ezborrow', 'ill'],
+    buttonIds: ['login', 'ezborrow', 'ill', 'afc'],
     buttonGenerators: {
       ezborrow: ({ item, config }) => {
         const title = item.pnx.addata.btitle ? item.pnx.addata.btitle[0] : '';
@@ -21,6 +21,10 @@ export default {
         label: 'Login to see request options',
         action: ($injector) => $injector.get('primoExploreCustomLoginService').login(),
       }),
+      afc: () => ({
+        label: "Schedule a video loan",
+        href: "https://nyu.qualtrics.com/jfe/form/SV_eKBzul896KmAWVL",
+      })
     },
     noButtonsText: '{item.request.blocked}',
     showButtons: {
@@ -35,8 +39,19 @@ export default {
         return !ezborrow && config.values.authorizedStatuses.ill.indexOf(user['bor-status']) > -1;
       },
       login: ({ user }) => user === undefined,
+      afc: ({ item, user, config }) => {
+        if (!user) return false;
+
+        const afcEligible = config.values.authorizedStatuses.afc.indexOf(user['bor-status']) > -1;
+
+        const isBAFCMainCollection = item.delivery.holding.some(({ subLocation, libraryCode}) => {
+          return libraryCode === "BAFC" && subLocation === "Main Collection";
+        });
+
+        return afcEligible && isBAFCMainCollection;
+      }
     },
-    hideDefaultRequest: ({ items, config, user }) => {
+    hideAllDefaultRequests: ({ items, config, user }) => {
       if (user === undefined) {
         return items.map(() => true);
       }
@@ -49,7 +64,18 @@ export default {
 
       return availabilityStatuses.map(isAvailable => allUnavailable || (itemsAreUnique && !isAvailable));
     },
-    hideCustomRequest: props => props.config.hideDefaultRequest(props).map(boolean => !boolean),
+    hideCustomRequests: ({ item, items, config, user}) => {
+      const hideExternalRequest = config.hideAllDefaultRequests({ items, config, user }).map(boolean => !boolean);
+      const hideAFC = !config.showButtons.afc({ item, user, config });
+      const loggedIn = user !== undefined;
+
+      return ({
+        ill: hideExternalRequest,
+        ezborrow: hideExternalRequest,
+        login: items.map(() => loggedIn),
+        afc: items.map(() => hideAFC),
+      });
+    },
     values: {
       baseUrls: {
         ezborrow: `https://${process.env.NODE_ENV !== 'production' ? 'dev' : ''}.login.library.nyu.edu/ezborrow/nyu`,
@@ -57,7 +83,8 @@ export default {
       },
       authorizedStatuses: {
         ezborrow: ["50", "51", "52", "53", "54", "55", "56", "57", "58", "60", "61", "62", "63", "65", "66", "80", "81", "82", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41"],
-        ill: ["30", "31", "32", "34", "35", "37", "50", "51", "52", "53", "54", "55", "56", "57", "58", "60", "61", "62", "63", "65", "66", "80", "81", "82"]
+        ill: ["30", "31", "32", "34", "35", "37", "50", "51", "52", "53", "54", "55", "56", "57", "58", "60", "61", "62", "63", "65", "66", "80", "81", "82"],
+        afc: ["03", "05", "10", "12", "20", "21", "30", "32", "50", "52", "53", "54", "61", "62", "70", "80", "89", "90"],
       },
       functions: {
         checkAreItemsUnique: items => items.some((item, _i, items) => item._additionalData.itemdescription !== items[0]._additionalData.itemdescription),
