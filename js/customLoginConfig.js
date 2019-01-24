c27e666dd4a0cc99b3233c210464d8d4181b545c
Elliott Grieco <elliott.grieco@nyu.edu>
2019-01-24 12:55:08 -0500

    Updates custom-login, custom-request

--- js/customLoginConfig.js
+++ js/customLoginConfig.js
@@ -4,5 +4,13 @@ export default {
     pdsUrl: `https://pds${process.env.NODE_ENV !== 'production' ? 'dev' : ''}.library.nyu.edu/pds`,
     queryString: 'func=get-attribute&attribute=bor_info',
     selectors: ['id', 'bor-status'],
+    mockConfig: {
+      enabled: true,
+      user: {
+        'id': '1234567',
+        'bor-status': '55',
+      },
+      delay: 500,
+    }
   },
 };
