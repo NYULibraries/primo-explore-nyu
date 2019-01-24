6e48068dff146a72c2b1a07ebc3e10f8b7875657
Elliott Grieco <elliott.grieco@nyu.edu>
2019-01-24 11:33:51 -0500

    Tidies custom-login config

--- js/customLoginConfig.js
+++ js/customLoginConfig.js
@@ -1,8 +1,8 @@
 export default {
+  name: 'primoExploreCustomLoginConfig',
   config: {
     pdsUrl: `https://pds${process.env.NODE_ENV !== 'production' ? 'dev' : ''}.library.nyu.edu/pds`,
     queryString: 'func=get-attribute&attribute=bor_info',
     selectors: ['id', 'bor-status'],
   },
-  name: 'primoExploreCustomLoginConfig',
 };
