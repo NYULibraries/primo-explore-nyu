3406f7d598b30ecfaa6fd19064b0bdfed7c1e899
Elliott Grieco <elliott.grieco@nyu.edu>
2019-02-20 16:53:57 -0500

    Indentation tweak [skip ci]

--- js/customActions.js
+++ js/customActions.js
@@ -13,23 +13,23 @@ export default {
       icon="ic_description_24px"
       icon-set="action"
       link="https://cite.library.nyu.edu/{pnx.search.recordid[0]}?calling_system=primo&institution=NYU&cite_to=endnote" />
-      <custom-action name="export_refworks"
-        label="RefWorks"
-        index=5
-        icon="ic_description_24px"
-        icon-set="action"
-        link="https://cite.library.nyu.edu/{pnx.search.recordid[0]}?calling_system=primo&institution=NYU&cite_to=refworks" />
-      <custom-action name="download_ris"
-        label="RIS"
-        index=6
-        icon="ic_file_download_24px"
-        icon-set="file"
-        link="https://cite.library.nyu.edu/{pnx.search.recordid[0]}?calling_system=primo&institution=NYU&cite_to=ris" />
-      <custom-action name="download_bibtex"
-        label="BibTeX"
-        index=7
-        icon="ic_file_download_24px"
-        icon-set="file"
-        link="https://cite.library.nyu.edu/{pnx.search.recordid[0]}?calling_system=primo&institution=NYU&cite_to=bibtex" />
+    <custom-action name="export_refworks"
+      label="RefWorks"
+      index=5
+      icon="ic_description_24px"
+      icon-set="action"
+      link="https://cite.library.nyu.edu/{pnx.search.recordid[0]}?calling_system=primo&institution=NYU&cite_to=refworks" />
+    <custom-action name="download_ris"
+      label="RIS"
+      index=6
+      icon="ic_file_download_24px"
+      icon-set="file"
+      link="https://cite.library.nyu.edu/{pnx.search.recordid[0]}?calling_system=primo&institution=NYU&cite_to=ris" />
+    <custom-action name="download_bibtex"
+      label="BibTeX"
+      index=7
+      icon="ic_file_download_24px"
+      icon-set="file"
+      link="https://cite.library.nyu.edu/{pnx.search.recordid[0]}?calling_system=primo&institution=NYU&cite_to=bibtex" />
   `,
 };
